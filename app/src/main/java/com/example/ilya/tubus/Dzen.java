47c787c59204685f8e2e79c43b072b73a508fdc5
Atrevalis <32127221+Atrevalis@users.noreply.github.com>
2018-05-25 19:33:20 +0500

    Update Dzen.java

--- app/src/main/java/com/example/ilya/tubus/Dzen.java
+++ app/src/main/java/com/example/ilya/tubus/Dzen.java
@@ -29,46 +29,55 @@ public class Dzen extends AppCompatActivity {
             Intent intent = new Intent(Dzen.this, Dzen_x.class);
             intent.putExtra("Dzen", "1");
             startActivity(intent);
+            finish();
         });
         X2.setOnClickListener(v -> {
             Intent intent = new Intent(Dzen.this, Dzen_x.class);
             intent.putExtra("Dzen", "2");
             startActivity(intent);
+            finish();
         });
         X3.setOnClickListener(v -> {
             Intent intent = new Intent(Dzen.this, Dzen_x.class);
             intent.putExtra("Dzen", "3");
             startActivity(intent);
+            finish();
         });
         X4.setOnClickListener(v -> {
             Intent intent = new Intent(Dzen.this, Dzen_x.class);
             intent.putExtra("Dzen", "4");
             startActivity(intent);
+            finish();
         });
         X5.setOnClickListener(v -> {
             Intent intent = new Intent(Dzen.this, Dzen_x.class);
             intent.putExtra("Dzen", "5");
             startActivity(intent);
+            finish();
         });
         X6.setOnClickListener(v -> {
             Intent intent = new Intent(Dzen.this, Dzen_x.class);
             intent.putExtra("Dzen", "6");
             startActivity(intent);
+            finish();
         });
         X7.setOnClickListener(v -> {
             Intent intent = new Intent(Dzen.this, Dzen_x.class);
             intent.putExtra("Dzen", "7");
             startActivity(intent);
+            finish();
         });
         X8.setOnClickListener(v -> {
             Intent intent = new Intent(Dzen.this, Dzen_x.class);
             intent.putExtra("Dzen", "8");
             startActivity(intent);
+            finish();
         });
         X9.setOnClickListener(v -> {
             Intent intent = new Intent(Dzen.this, Dzen_x.class);
             intent.putExtra("Dzen", "9");
             startActivity(intent);
+            finish();
         });
         menu.setOnClickListener(v -> {
             Intent intent = new Intent(Dzen.this, Menu.class);
